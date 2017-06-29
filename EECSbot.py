c9fb3e64be211daf196e5e402ab970900e809ae0
eashman123 <eashaank@gmail.com>
2017-06-29 12:19:17 +0530

    Added cs copypasta

--- EECSbot.py
+++ EECSbot.py
@@ -36,6 +36,7 @@ async def reddit_checker():
             if newusersub(i):
                 for j in range(len(userinfo[i][1])):
                     await client.send_message(client.get_channel(userinfo[i][0][0]), userinfo[i][1][j])
+                await client.send_message(client.get_channel(userinfo[i][0][0]), '...........................................................')
         await asyncio.sleep(60)
 
 @client.event
@@ -48,9 +49,12 @@ Realistically the Berkeley EECS Force would be far more effective than half the 
 I don't actually support the violent overthrow of Stanford. I just think its kind of a fun idea conceptualy.''', '''First they came for Womens' Studies and I did not speak out because I wasn't a hippie
 Then they came for Pre-med and I did not speak out because I didn't want to be a doctor
 Then they came for Pre-Haas and I did not speak out because I had no interest in business
-Finally they came for me and the rest of the EECS majors were too socially awkward to speak for me''', '''I'd just like to interject for a moment. What you're referring to as CS is actually EE+CS or as I've recently taken to calling it, EECS. CS is not a field all to itself but rather a subdivision of computing theory only made practical by advancements in digital circuitry. Many people today study CS without recognizing its roots in digital logic and signal processing.Through a particular turn of events, the most direct means of interaction most students have with computers today is through programming, and so many are not aware of the fundamental role of electrical engineering in designing them.
-There really is a CS and these people are majoring in it, but it is just another layer on top of a more nuanced view of computing. CS is the theory of computation: the field dealing with abstract algorithms and data structures necessary to implement certain logic. CS is a necessary field, but its mathematical theory is useless by itself; it can only be put into practice with the help of microchips designed by electrical engineers to transform analog signals into discrete logic. Practical CS is normally reliant upon on a computer built upon layers and layers of circuitry. The whole field is basically CS built on top of EE, or EECS. All the so-called CS majors are really abstracted EE majors.''']#im a horrible person
+Finally they came for me and the rest of the EECS majors were too socially awkward to speak for me''']#im a horrible person
         await client.send_message(message.channel, choice(eecs_copypasta))
+    elif 'cs' in message.content and 'meme' in str(message.channel):
+        cs_copypasta = '''I'd just like to interject for a moment. What you're referring to as CS is actually EE+CS or as I've recently taken to calling it, EECS. CS is not a field all to itself but rather a subdivision of computing theory only made practical by advancements in digital circuitry. Many people today study CS without recognizing its roots in digital logic and signal processing.Through a particular turn of events, the most direct means of interaction most students have with computers today is through programming, and so many are not aware of the fundamental role of electrical engineering in designing them.
+There really is a CS and these people are majoring in it, but it is just another layer on top of a more nuanced view of computing. CS is the theory of computation: the field dealing with abstract algorithms and data structures necessary to implement certain logic. CS is a necessary field, but its mathematical theory is useless by itself; it can only be put into practice with the help of microchips designed by electrical engineers to transform analog signals into discrete logic. Practical CS is normally reliant upon on a computer built upon layers and layers of circuitry. The whole field is basically CS built on top of EE, or EECS. All the so-called CS majors are really abstracted EE majors.'''
+        await client.send_message(message.channel, choice(cs_copypasta))
     await client.process_commands(message)
 
 @client.command(description='Track comments or submissions by redditor. Defaults to submissions.', pass_context=True)
